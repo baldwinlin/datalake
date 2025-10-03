fe4a0e12e5134dcf76e4715c61e6a0354e43fef5
Rover <rover@RoverdeMacBook-Pro.local>
2025-10-03 18:28:11 +0800

    AL

--- dao/impl/S3DaoImpl.py
+++ dao/impl/S3DaoImpl.py
@@ -75,22 +75,22 @@ class S3DaoImpl(FileDao):
         except Exception as e:
             raise Exception(f"S3 列出檔案失敗: {e}")
 
-    # def listFilesWithoutFolder(self, pattern: str) -> List[str]:
-    #     try:
-    #         paginator = self.s3.get_paginator('list_objects_v2')
-    #         page_iterator = paginator.paginate(Bucket=self.bucket)
+    def listFilesWithoutFolder(self, pattern: str, prefix: str) -> List[str]:
+        try:
+            paginator = self.s3.get_paginator('list_objects_v2')
+            page_iterator = paginator.paginate(Bucket=self.bucket, Prefix=prefix, Delimiter='/')
 
-    #         files = []
-    #         for page in page_iterator:
-    #             for obj in page.get('Contents', []):
-    #                 key = obj['Key']
-    #                 if key.endswith('/'):
-    #                     continue
-    #                 if FilenameProcessor._match_name_pattern(key, pattern):
-    #                     files.append(key)
-    #         return files
-    #     except Exception as e:
-    #         raise Exception(f"S3 列出檔案失敗: {e}")
+            files = []
+            for page in page_iterator:
+                for obj in page.get('Contents', []):
+                    key = obj['Key']
+                    if key.endswith('/'):
+                        continue
+                    if FilenameProcessor._match_name_pattern(key, pattern):
+                        files.append(key)
+            return files
+        except Exception as e:
+            raise Exception(f"S3 列出檔案失敗: {e}")
     
     def listFilesWithDate(self, pattern: str, prefix: str) -> List[str]:
         try:
